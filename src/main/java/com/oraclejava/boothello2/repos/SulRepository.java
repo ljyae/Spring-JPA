80409483855cfa2a5ff2356827a5da24d98cf596
Jiye Lee <60422182+ljyae@users.noreply.github.com>
2021-11-22 16:19:18 +0900

    Delete src/main directory

--- src/main/java/com/oraclejava/boothello2/repos/SulRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oraclejava.boothello2.repos;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.oraclejava.boothello2.entities.Sul;
-
-public interface SulRepository extends PagingAndSortingRepository<Sul, Integer> {
-
-}
--- src/main/java/com/oraclejava/boothello2/repos/SulTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oraclejava.boothello2.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.oraclejava.boothello2.entities.SulType;
-
-public interface SulTypeRepository extends CrudRepository<SulType, Integer>{
-
-}
